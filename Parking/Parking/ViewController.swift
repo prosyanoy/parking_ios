c2f124a3a2b0e8c6fb61d918d98b3f29ab2e6107
Sofia Lupeko <sonya.lupeko@mail.ru>
2022-07-23 20:00:43 +0300

    Инициализация проекта

---
+++ Parking/Parking/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Parking
+//
+//  Created by Sofia Lupeko on 23.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
