6164bcebac15f437628dcff281a8da596317a132
Max <4440449@gmail.com>
2022-07-26 14:21:34 +0300

    Отрефакторил декодирование

--- Parking/Parking/Data/Gateways/Parking/Network/Entity/ParkingNetworkEntity.swift
+++ Parking/Parking/Data/Gateways/Parking/Network/Entity/ParkingNetworkEntity.swift
@@ -10,6 +10,7 @@ import Foundation
 
 // Error
 enum ParkingNetworkEntityError: Error {
+    case failedDecoding(String)
     case parseToDomain(String)
 }
 
@@ -25,7 +26,7 @@ struct ParkingNetworkEntity: Codable,
                              DomainConvertable {
     
     let id: String
-    let coordinates: String // [String : Any] - not valid decoding type
+    let coordinates: CoordinatesNetworkEntity
     let adress: String
     let hourCost: String
     
@@ -50,9 +51,12 @@ struct ParkingNetworkEntity: Codable,
     init(from decoder: Decoder) throws {
         do {
             let container = try decoder.container(keyedBy: CodingKeys.self)
+            let jsonCoordinates = try container.decode(String.self, forKey: .coordinates)
+            guard let dataCoordinates = jsonCoordinates.data(using: .utf8) else {
+                throw ParkingNetworkEntityError.failedDecoding("failed json data encoding")
+            }
+            self.coordinates = try JSONDecoder().decode(CoordinatesNetworkEntity.self, from: dataCoordinates)
             self.id = try container.decode(String.self, forKey: .id)
-            //Coordinates парсится через доп сериализацию JsonObject
-            self.coordinates = try container.decode(String.self, forKey: .coordinates)
             self.adress = try container.decode(String.self, forKey: .address)
             self.hourCost = try container.decode(String.self, forKey: .hour_cost)
         } catch let error {
@@ -68,47 +72,58 @@ struct ParkingNetworkEntity: Codable,
                   let hourCost = Float(self.hourCost) else {
                       throw ParkingNetworkEntityError.parseToDomain("invalid string format")
                   }
-            guard let data = self.coordinates.data(using: .utf8) else {
-                throw ParkingNetworkEntityError.parseToDomain("invalid data encoding")
-            }
-            guard let jsonArray = try JSONSerialization.jsonObject(with: data, options: []) as? [String : Any] else {
-                throw ParkingNetworkEntityError.parseToDomain("invalid data format")
-            }
-            var coordinates = Parking.Coordinates(point: [0],
-                                                 type: "",
-                                                 form: [[0]])
-            try jsonArray.forEach { key, _ in
-                if key == "point" {
-                    if let point = jsonArray["point"] as? [Double] {
-                        coordinates.point = point
-                    } else {
-                        throw ParkingNetworkEntityError.parseToDomain("invalid json format")
-                    }
-                } else if key == "type" {
-                    if let type = jsonArray["type"] as? String {
-                        coordinates.type = type
-                    } else {
-                        throw ParkingNetworkEntityError.parseToDomain("invalid json format")
-                    }
-                } else if key == "list" {
-                    if let form = jsonArray["list"] as? [[Double]] {
-                        coordinates.form = form
-                    } else {
-                        throw ParkingNetworkEntityError.parseToDomain("invalid json format")
-                    }
-                }
-            }
-            
-            let parking = Parking(id: id,
-                                  coordinates: coordinates,
-                                  adress: self.adress,
-                                  hourCost: hourCost)
-            print("parking == \(parking) \n \n")
-            return parking
-            
+            let coordinates = coordinates.parseToDomain()
+            return .init(id: id,
+                         coordinates: coordinates,
+                         adress: self.adress,
+                         hourCost: hourCost)
         } catch let error {
             throw error
         }
     }
     
+    
+    
+    struct CoordinatesNetworkEntity: Codable,
+                                     DomainConvertable {
+        let point: [Double]
+        let type: String
+        let list: [[Double]]
+        
+        enum CodingKeys: CodingKey {
+            case point
+            case type
+            case list
+        }
+        
+        // MARK: Codable
+        func encode(to encoder: Encoder) throws {
+            var container = encoder.container(keyedBy: CodingKeys.self)
+            try container.encode(point, forKey: .point)
+            try container.encode(type, forKey: .type)
+            try container.encode(list, forKey: .list)
+        }
+        
+        // MARK: Decodable
+        init(from decoder: Decoder) throws {
+            do {
+                let container = try decoder.container(keyedBy: CodingKeys.self)
+                self.point = try container.decode([Double].self, forKey: .point)
+                self.type = try container.decode(String.self, forKey: .type)
+                self.list = try container.decode([[Double]].self, forKey: .list)
+            } catch let error {
+                throw error
+            }
+        }
+        
+        // MARK: DomainConvertable
+        func parseToDomain() -> Parking.Coordinates {
+            return .init(point: self.point,
+                         type: self.type,
+                         form: self.list)
+        }
+    }
+    
 }
+
+
