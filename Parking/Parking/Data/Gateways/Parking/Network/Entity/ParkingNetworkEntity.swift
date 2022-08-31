c66e890880e0dbd4471872d8a124bdd8182d7a05
Max <4440449@gmail.com>
2022-08-31 17:33:25 +0300

    Обновлена модель Парковки + ее парсинг с сервера / Также исправлен баг на Карте, при котором отрисованный без Кластера Плейсмарк не реагировал на нажатие

--- Parking/Parking/Data/Gateways/Parking/Network/Entity/ParkingNetworkEntity.swift
+++ Parking/Parking/Data/Gateways/Parking/Network/Entity/ParkingNetworkEntity.swift
@@ -15,80 +15,110 @@ enum ParkingNetworkEntityError: Error {
 }
 
 
-
 protocol DomainConvertable {
     associatedtype DomainEntity
     func parseToDomain() throws -> DomainEntity
 }
 
 
-struct ParkingNetworkEntity: Codable,
+struct ParkingNetworkEntity: Decodable,
                              DomainConvertable {
-    
     let id: String
     let coordinates: CoordinatesNetworkEntity
     let adress: String
     let hourCost: String
+    
+    let isClosed: String
+    let covered: String
+    let secure: String
+    let aroundTheClock: String
+    let ev: String
+    let disabled: String
+    
+    let costPerHour: [[String : String]]
+    let time: [[WorkScheduleNetworkEntity]]
     
     enum CodingKeys: CodingKey {
         case id
         case coordinates
         case address
         case hour_cost
+        
+        case isClosed
+        case covered
+        case secure
+        case around_the_clock
+        case ev
+        case disabled
+        
+        case cost_per_hour
+        case time
     }
-    
-    // MARK: Codable
-    func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: CodingKeys.self)
-        try container.encode(id, forKey: .id)
-        try container.encode(coordinates, forKey: .coordinates)
-        try container.encode(adress, forKey: .address)
-        try container.encode(hourCost, forKey: .hour_cost)
-    }
-    
     
     // MARK: Decodable
     init(from decoder: Decoder) throws {
         do {
             let container = try decoder.container(keyedBy: CodingKeys.self)
-            let jsonCoordinates = try container.decode(String.self, forKey: .coordinates)
-            guard let dataCoordinates = jsonCoordinates.data(using: .utf8) else {
-                throw ParkingNetworkEntityError.failedDecoding("failed json data encoding")
-            }
-            self.coordinates = try JSONDecoder().decode(CoordinatesNetworkEntity.self, from: dataCoordinates)
+            
             self.id = try container.decode(String.self, forKey: .id)
+            self.coordinates = try container.decode(CoordinatesNetworkEntity.self, forKey: .coordinates)
             self.adress = try container.decode(String.self, forKey: .address)
             self.hourCost = try container.decode(String.self, forKey: .hour_cost)
+            
+            self.isClosed = try container.decode(String.self, forKey: .isClosed)
+            self.covered = try container.decode(String.self, forKey: .covered)
+            self.secure = try container.decode(String.self, forKey: .secure)
+            self.aroundTheClock = try container.decode(String.self, forKey: .around_the_clock)
+            self.ev = try container.decode(String.self, forKey: .ev)
+            self.disabled = try container.decode(String.self, forKey: .disabled)
+            
+            self.costPerHour = try container.decode([[String : String]].self, forKey: .cost_per_hour)
+            self.time = try container.decode([[WorkScheduleNetworkEntity]].self, forKey: .time)
         } catch let error {
             throw error
         }
     }
-    
     
     // MARK: DomainConvertable
     func parseToDomain() throws -> Parking {
         do {
             guard let id = Int(self.id),
                   let hourCost = Float(self.hourCost) else {
-                      throw ParkingNetworkEntityError.parseToDomain("invalid string format")
-                  }
+                throw ParkingNetworkEntityError.parseToDomain("invalid Parking string format")
+            }
+            let isClosed = NSString(string: self.isClosed).boolValue
+            let covered = NSString(string: self.covered).boolValue
+            let secure = NSString(string: self.secure).boolValue
+            let aroundTheClock = NSString(string: self.aroundTheClock).boolValue
+            let ev = NSString(string: self.ev).boolValue
+            let disabled = NSString(string: self.disabled).boolValue
+            
             let coordinates = try coordinates.parseToDomain()
+            let workSchedule = try time.map { try $0.map { try $0.parseToDomain() } }
             return .init(id: id,
                          coordinates: coordinates,
                          adress: self.adress,
-                         hourCost: hourCost)
+                         hourCost: hourCost,
+                         isClosed: isClosed,
+                         covered: covered,
+                         secure: secure,
+                         aroundTheClock: aroundTheClock,
+                         ev: ev,
+                         disabled: disabled,
+                         billing: self.costPerHour,
+                         workSchedule: workSchedule)
         } catch let error {
             throw error
         }
     }
     
     
-    
     struct CoordinatesNetworkEntity: Codable,
                                      DomainConvertable {
-        let point: [Double]
+        // TODO: нужен дабл с сервера!!
+        let point: [String] // Double
         let type: String
-        let list: [[Double]]
+        let list: [[String]] // Double
         
         enum CodingKeys: CodingKey {
             case point
@@ -108,9 +138,9 @@ struct ParkingNetworkEntity: Codable,
         init(from decoder: Decoder) throws {
             do {
                 let container = try decoder.container(keyedBy: CodingKeys.self)
-                self.point = try container.decode([Double].self, forKey: .point)
+                self.point = try container.decode([String].self, forKey: .point)
                 self.type = try container.decode(String.self, forKey: .type)
-                self.list = try container.decode([[Double]].self, forKey: .list)
+                self.list = try container.decode([[String]].self, forKey: .list)
             } catch let error {
                 throw error
             }
@@ -119,13 +149,57 @@ struct ParkingNetworkEntity: Codable,
         // MARK: DomainConvertable
         func parseToDomain() throws -> Parking.Coordinates {
             guard let type = Parking.CoordinateType(rawValue: self.type) else {
-                throw ParkingNetworkEntityError.parseToDomain("invalid Coordinate type --> \(self.type)") }
-            return .init(point: self.point,
+                throw ParkingNetworkEntityError.parseToDomain("invalid Coordinate type --> \(self.type)")
+            }
+            // TODO: нужен дабл с сервера!!
+            let doublePoint = self.point.compactMap { Double($0) }
+            let doubleList = self.list.compactMap { $0.compactMap { Double($0) } }
+            return .init(point: doublePoint,
                          type: type,
-                         form: self.list)
+                         form: doubleList)
+        }
+    }
+    
+    
+    struct WorkScheduleNetworkEntity: Decodable,
+                                      DomainConvertable {
+        let days: String
+        let openingTime: String
+        let closingTime: String
+        let isWorkingToday: String
+        
+        enum CodingKeys: CodingKey {
+            case days
+            case open
+            case close
+            case isWorking
+        }
+        
+        // MARK: Decodable
+        init(from decoder: Decoder) throws {
+            do {
+                let container = try decoder.container(keyedBy: CodingKeys.self)
+                self.days = try container.decode(String.self, forKey: .days)
+                self.openingTime = try container.decode(String.self, forKey: .open)
+                self.closingTime = try container.decode(String.self, forKey: .close)
+                self.isWorkingToday = try container.decode(String.self, forKey: .isWorking)
+            } catch let error {
+                throw error
+            }
+        }
+        
+        // MARK: DomainConvertable
+        func parseToDomain() throws -> Parking.WorkSchedule {
+            guard let day = Parking.WorkSchedule.Weekday(rawValue: self.days) else {
+                throw ParkingNetworkEntityError.parseToDomain("invalid WorkSchedule input type")
+            }
+            let isWorkingToday = NSString(string: self.isWorkingToday).boolValue
+            
+            return .init(days: day,
+                         openingTime: self.openingTime,
+                         closingTime: self.closingTime,
+                         isWorkingToday: isWorkingToday)
         }
     }
     
 }
-
-
--- Parking/Parking/Domain/Entities/Parking/Parking.swift
+++ Parking/Parking/Domain/Entities/Parking/Parking.swift
@@ -5,6 +5,7 @@
 //  Created by Maxim on 25.07.2022.
 //
 
+
 struct Parking {
     
     let id: Int
@@ -21,6 +22,34 @@ struct Parking {
     enum CoordinateType: String {
         case polyGon = "g"
         case polyLine = "l"
+    }
+
+    let isClosed: Bool
+    let covered: Bool
+    let secure: Bool
+    let aroundTheClock: Bool
+    let ev: Bool
+    let disabled: Bool
+    // тарификация и график работы биндятся между собой по соотнощению массивов 1:1 или 2:2 или 3:3, максимум 7:7, т.е. каждому массиву с днями недели, соответствует каждый массив тарификации с тем же индексом
+    // Пока так!
+    let billing: [[String : String]]
+    let workSchedule: [[WorkSchedule]]
+    
+    struct WorkSchedule {
+        let days: Weekday
+        let openingTime: String
+        let closingTime: String
+        let isWorkingToday: Bool
+        
+        enum Weekday: String {
+            case monday = "1"
+            case tuesday = "2"
+            case wednesday = "3"
+            case thursday = "4"
+            case friday = "5"
+            case saturday = "6"
+            case sunday = "7"
+        }
     }
     
 }
