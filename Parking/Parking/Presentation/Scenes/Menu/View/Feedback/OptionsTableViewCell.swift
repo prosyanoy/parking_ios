2f3f09b819045a125a5afd93fa1fbb1dbee1ff5d
Nikita1801 <nikita-makarevich@mail.ru>
2022-09-11 18:41:01 +0300

    feedback

--- Parking/Parking/Presentation/Scenes/Menu/View/Feedback/OptionsTableViewCell.swift
+++ Parking/Parking/Presentation/Scenes/Menu/View/Feedback/OptionsTableViewCell.swift
@@ -7,17 +7,15 @@
 
 import UIKit
 
-class OptionsTableViewCell: UITableViewCell {
+final class OptionsTableViewCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
-
+    
 }
