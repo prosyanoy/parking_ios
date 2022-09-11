507824949d52b23318f5c354e6793365dd32a2fb
Nikita1801 <nikita-makarevich@mail.ru>
2022-09-11 18:40:25 +0300

    feedback

---
+++ Parking/Parking/Presentation/Scenes/Menu/View/Feedback/OptionsTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  OptionsTableViewCell.swift
+//  Parking
+//
+//  Created by Никита Макаревич on 08.09.2022.
+//
+
+import UIKit
+
+class OptionsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
