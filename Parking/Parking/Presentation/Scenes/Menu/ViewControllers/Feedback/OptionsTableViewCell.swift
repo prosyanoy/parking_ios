35ea3d16f9afe31abf2c2b725551662c1308d3df
Nikita1801 <nikita-makarevich@mail.ru>
2022-09-12 20:03:45 +0300

    feedback

---
+++ Parking/Parking/Presentation/Scenes/Menu/ViewControllers/Feedback/OptionsTableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  OptionsTableViewCell.swift
+//  Parking
+//
+//  Created by Никита Макаревич on 08.09.2022.
+//
+import UIKit
+
+final class OptionsTableViewCell: UITableViewCell {
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
